7c38ab45091e800413209731e4f13c6396818059
quocphongg07 <quocphongg07@gmail.com>
2024-04-04 23:47:09 +0700

    Hello

--- src/main/java/quanlythuvien/views/LoginView.java
+++ src/main/java/quanlythuvien/views/LoginView.java
@@ -1,7 +1,10 @@
 package quanlythuvien.views;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,17 +14,20 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
 import quanlythuvien.entities.User;
 
 public class LoginView extends JFrame implements ActionListener {
-    private static final long serialVersionUID = 1L;
+//    private static final long serialVersionUID = 1L;
     private JLabel userNameLabel;
     private JLabel passwordlabel;
+    private JLabel title;
     private JPasswordField passwordField;
     private JTextField userNameField;
     private JButton loginBtn;
+    private JLabel img;
 
     public LoginView() {
         initComponents();
@@ -31,43 +37,63 @@ public class LoginView extends JFrame implements ActionListener {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         userNameLabel = new JLabel("UserName");
         passwordlabel = new JLabel("Password");
+        title = new JLabel("ỨNG DỤNG QUẢN LÝ ẤN PHẨM THƯ VIỆN");
+        img = new JLabel();
         userNameField = new JTextField(15);
         passwordField = new JPasswordField(15);
         loginBtn = new JButton();
+        loginBtn.setBackground(Color.darkGray);
+        loginBtn.setForeground(Color.white);
+        
+        userNameLabel.setForeground(Color.white);
+        passwordlabel.setForeground(Color.white);
+        title.setForeground(Color.white);
+        
+        title.setFont(new Font("Serif", Font.PLAIN, 25));
+        
+        img.setHorizontalAlignment(SwingConstants.LEFT);
+        img.setBounds(0, 0, 400, 300);
+        img.setIcon(new ImageIcon("background.jpg"));
 
         loginBtn.setText("Login");
         loginBtn.addActionListener(this);
-
-        // tạo spring layout
+//
+//        // tạo spring layout
         SpringLayout layout = new SpringLayout();
         JPanel panel = new JPanel();
         // tạo đối tượng panel để chứa các thành phần của màn hình login
-        panel.setSize(400, 300);
+        panel.setSize(300, 200);
         panel.setLayout(layout);
         panel.add(userNameLabel);
         panel.add(passwordlabel);
+        panel.add(title);
         panel.add(userNameField);
         panel.add(passwordField);
         panel.add(loginBtn);
+        panel.add(img);
+
+        setLocationRelativeTo(null);
 
         // cài đặt vị trí các thành phần trên màn hình login
-        layout.putConstraint(SpringLayout.WEST, userNameLabel, 20, SpringLayout.WEST, panel);
-        layout.putConstraint(SpringLayout.NORTH, userNameLabel, 80, SpringLayout.NORTH, panel);
-        layout.putConstraint(SpringLayout.WEST, passwordlabel, 20, SpringLayout.WEST, panel);
-        layout.putConstraint(SpringLayout.NORTH, passwordlabel, 105, SpringLayout.NORTH, panel);
-        layout.putConstraint(SpringLayout.WEST, userNameField, 80, SpringLayout.WEST, userNameLabel);
-        layout.putConstraint(SpringLayout.NORTH, userNameField, 80, SpringLayout.NORTH, panel);
-        layout.putConstraint(SpringLayout.WEST, passwordField, 80, SpringLayout.WEST, passwordlabel);
-        layout.putConstraint(SpringLayout.NORTH, passwordField, 105, SpringLayout.NORTH, panel);
-        layout.putConstraint(SpringLayout.WEST, loginBtn, 80, SpringLayout.WEST, passwordlabel);
-        layout.putConstraint(SpringLayout.NORTH, loginBtn, 130, SpringLayout.NORTH, panel);
+        layout.putConstraint(SpringLayout.WEST, userNameLabel, 100, SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, userNameLabel, 200, SpringLayout.NORTH, panel);
+        layout.putConstraint(SpringLayout.WEST, passwordlabel, 100, SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, passwordlabel, 230, SpringLayout.NORTH, panel);
+        layout.putConstraint(SpringLayout.WEST, title, 50, SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, title, 100, SpringLayout.NORTH, panel);
+        layout.putConstraint(SpringLayout.WEST, userNameField, 100, SpringLayout.WEST, userNameLabel);
+        layout.putConstraint(SpringLayout.NORTH, userNameField, 200, SpringLayout.NORTH, panel);
+        layout.putConstraint(SpringLayout.WEST, passwordField, 100, SpringLayout.WEST, passwordlabel);
+        layout.putConstraint(SpringLayout.NORTH, passwordField, 230, SpringLayout.NORTH, panel);
+        layout.putConstraint(SpringLayout.WEST, loginBtn, 150, SpringLayout.WEST, passwordlabel);
+        layout.putConstraint(SpringLayout.NORTH, loginBtn, 280, SpringLayout.NORTH, panel);
 
-        // add panel tới JFrame
+//        // add panel tới JFrame
         this.add(panel);
-        this.pack();
-        // cài đặt các thuộc tính cho JFrame
+
+//        // cài đặt các thuộc tính cho JFrame
         this.setTitle("Login");
-        this.setSize(400, 300);
+        this.setSize(600, 400);
         this.setResizable(false);
     }
 
